6f148d96e5d5ff5995bd3e7dd60e8a85cd0059a4
Natavan Nabizada <55505907+NatavanNabizada@users.noreply.github.com>
2024-01-17 11:06:59 +0400

    Add files via upload

---
+++ src/main/java/eventbrakerdashboardapi/az/EventbrakerDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package eventbrakerdashboardapi.az;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbrakerDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbrakerDashboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eventbrakerdashboardapi/az/EventbrakerDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eventbrakerdashboardapi.az;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbrakerDashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
